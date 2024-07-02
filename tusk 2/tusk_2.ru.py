f33eb45d479603aea75dfe95a4e35010e2bdcd4a
Cem745 <cemen.zagumenow@gmail.com>
2024-07-02 08:41:57 +0300

    Add files via upload

---
+++ tusk 2/tusk_2.ru.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+if __name__ == '__main__':
+    curcle = input('Путь до файла с координатами и радиусом окружности: ')
+    dot = input('Путь до файла с координатами точек: ')
+    with open(curcle) as f:
+        xc, yc = f.readline().split()
+        xc, yc = float(xc), float(yc)
+        r = int(f.readline(2))
+    with open(dot) as f:
+        points = f.readlines()
+    for point in points:
+        x = float(point.split()[0])
+        y = float(point.split()[1])
+        s = sqrt((xc - x) ** 2 + (yc - y) ** 2)
+        if s == r:
+            print(0)
+        elif s < r:
+            print(1)
+        else:
+            print(2)
